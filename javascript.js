d14078ff10877f3b7e0067d3b90069f3179648d1
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-21 23:42:40 +0200

    Add basic arithmetic functions

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// functions for add sub mul div
+function add(first,second) {return first + second}
+function subtract(first,second) {return first - second}
+function multiply(first,second) {return first * second}
+function divide(first,second) {return first / second}
