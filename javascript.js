38a6a509faea6a33c33abe766c54d52d9ff22fe2
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-21 23:48:23 +0200

    Add operate()

--- javascript.js
+++ javascript.js
@@ -3,3 +3,16 @@ function add(first,second) {return first + second}
 function subtract(first,second) {return first - second}
 function multiply(first,second) {return first * second}
 function divide(first,second) {return first / second}
+
+function operate(first, second, operator) {
+    if (operator === "+") {return add(first,second)}
+    else if (operator === "-") {return subtract(first,second)}
+    else if (operator === "*") {return multiply(first,second)}
+    else if (operator === "/") {return divide(first,second)}
+
+}
+
+
+let first;
+let second;
+let operator;
