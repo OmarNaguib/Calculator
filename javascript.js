ecaf6dd5aabc7bf77b0cc826d6929e5614980168
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-22 05:54:19 +0200

    Add number listeners

--- javascript.js
+++ javascript.js
@@ -1,18 +1,46 @@
 // functions for add sub mul div
-function add(first,second) {return first + second}
-function subtract(first,second) {return first - second}
-function multiply(first,second) {return first * second}
-function divide(first,second) {return first / second}
+function add(first,second) {return +first + +second;}
+function subtract(first,second) {return first - second;}
+function multiply(first,second) {return first * second;}
+function divide(first,second) {return first / second;}
 
 function operate(first, second, operator) {
-    if (operator === "+") {return add(first,second)}
-    else if (operator === "-") {return subtract(first,second)}
-    else if (operator === "*") {return multiply(first,second)}
-    else if (operator === "/") {return divide(first,second)}
+    if (operator === "+") {return add(first,second);}
+    else if (operator === "-") {return subtract(first,second);}
+    else if (operator === "*") {return multiply(first,second);}
+    else if (operator === "/") {return divide(first,second);}
+
+}
+
+function setPointer(value) {data.pointer=value}
+
+
+numbers=document.querySelectorAll(".number");
+numbers.forEach(number => { number.addEventListener("click",()=> {
+    if (data.result) {data.clear()}
+    data[data.pointer]+=number.textContent
+    console.table(data)
+})
+});
+
+
+
+data = {
+    first: "",
+    second: "",
+    operator:"",
+    pointer :"first",
+    clear() {
+        this.first="";
+        this.second="";
+        this.operator="";
+        this.pointer="first";
+        delete this.result
+    }
+    
 
 }
 
 
-let first;
-let second;
-let operator;
+
+
