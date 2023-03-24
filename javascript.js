5c4e94faa060e165e7eda1812a416406d3465395
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-24 06:11:45 +0200

    bigger display

--- javascript.js
+++ javascript.js
@@ -33,7 +33,6 @@ function clearData() {
 }
 
 
-// functions for add sub mul div
 function add(first,second) {return +first + +second;}
 function subtract(first,second) {return first - second;}
 function multiply(first,second) {return first * second;}
